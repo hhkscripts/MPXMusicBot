c093bc161602371b503b9c29fe3abb5af066e0d8
h2kyaw <mr.kiddomonster@gmail.com>
2024-11-13 15:33:37 +0630

    Plugins: Speedtest fix

--- MPXMusic/plugins/tools/speedtest.py
+++ MPXMusic/plugins/tools/speedtest.py
@@ -1,7 +1,5 @@
 import asyncio
-
 from pyrogram import filters
-
 import speedtest
 from MPXMusic import app
 from MPXMusic.misc import SUDOERS
@@ -9,28 +7,31 @@ from strings import get_command
 
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
 
-
 def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("⇆ Testando **download** ... ⬇️")
+        m.edit("⇆ Testando **download** ... ⬇️")
         test.download()
-        m = m.edit("⇆ Testando **upload** ... ⬆️")
+        m.edit("⇆ Testando **upload** ... ⬆️")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("↻ Compartilhando os resultados do SpeedTest ... 📊")
+        m.edit("↻ Compartilhando os resultados do SpeedTest ... 📊")
     except Exception as e:
-        return m.edit(f"⚠️ Erro: {e}")
+        return {"error": str(e)}
     return result
-
 
 @app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
 async def speedtest_function(client, message):
     m = await message.reply_text("🚀 **Iniciando SpeedTest**...")
     loop = asyncio.get_event_loop_policy().get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
+    
+    if "error" in result:
+        await m.edit(f"⚠️ Erro: {result['error']}")
+        return
+
     output = f"""**Resultados do SpeedTest** 📊
 
 <u>**Cliente:**</u>
@@ -43,7 +44,10 @@ async def speedtest_function(client, message):
 💼 **Patrocinador:** {result['server']['sponsor']}
 ⚡ **Latência:** {result['server']['latency']} ms  
 🏓 **Ping :** {result['ping']} ms"""
-    msg = await app.send_photo(
-        chat_id=message.chat.id, photo=result["share"], caption=output
+
+    await app.send_photo(
+        chat_id=message.chat.id, 
+        photo=result["share"], 
+        caption=output
     )
     await m.delete()
