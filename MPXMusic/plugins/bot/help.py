293857853a6908d7318a5ace118a06e3c813814c
h2kyaw <mr.kiddomonster@gmail.com>
2024-11-13 12:29:01 +0630

    reply_photo: testing Images;

--- MPXMusic/plugins/bot/help.py
+++ MPXMusic/plugins/bot/help.py
@@ -22,6 +22,20 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 COLUMN_SIZE = 4  # number of  button height
 NUM_COLUMNS = 3  # number of button width
 
+AVISHA = [
+    "https://graph.org/file/eaa3a2602e43844a488a5.jpg",
+    "https://graph.org/file/b129e98b6e5c4db81c15f.jpg",
+    "https://graph.org/file/3ccb86d7d62e8ee0a2e8b.jpg",
+    "https://graph.org/file/df11d8257613418142063.jpg",
+    "https://graph.org/file/9e23720fedc47259b6195.jpg",
+    "https://graph.org/file/826485f2d7db6f09db8ed.jpg",
+    "https://graph.org/file/ff3ad786da825b5205691.jpg",
+    "https://graph.org/file/52713c9fe9253ae668f13.jpg",
+    "https://graph.org/file/8f8516c86677a8c91bfb1.jpg",
+    "https://graph.org/file/6603c3740378d3f7187da.jpg",
+    "https://graph.org/file/66cb6ec40eea5c4670118.jpg",
+    "https://graph.org/file/2e3cf4327b169b981055e.jpg",
+]
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other: InlineKeyboardButton):
@@ -129,7 +143,7 @@ async def helper_private(
         )
         if START_IMG_URL:
             await update.reply_photo(
-                photo=START_IMG_URL,
+                random.choice(AVISHA),
                 caption=_["help_1"],
                 reply_markup=keyboard,
             )
