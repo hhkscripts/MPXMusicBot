d2d18454322293b59dec4a1dce8656d8cb85eb7f
h2kyaw <mr.kiddomonster@gmail.com>
2024-11-13 08:26:17 +0630

    Config: Update.

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 # For customized or modified Repository
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/hhkscripts/MPXMusicBot",)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "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",)
