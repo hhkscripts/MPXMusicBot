93a6b4ce90924d5e89e4390192e8ce0dbf9f6ac7
h2kyaw <mr.kiddomonster@gmail.com>
2024-11-13 12:34:03 +0630

    Start Image: Test

--- MPXMusic/plugins/bot/start.py
+++ MPXMusic/plugins/bot/start.py
@@ -7,11 +7,11 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from MPXMusic import HELPABLE, app, Platform
-from MPXMusic.misc import SUDOERS, _boot_
-from MPXMusic.plugins.play.playlist import del_plist_msg
-from MPXMusic.plugins.sudo.sudoers import sudoers_list
-from MPXMusic.utils.database import (
+from WinxMusic import HELPABLE, app, Platform
+from WinxMusic.misc import SUDOERS, _boot_
+from WinxMusic.plugins.play.playlist import del_plist_msg
+from WinxMusic.plugins.sudo.sudoers import sudoers_list
+from WinxMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -21,10 +21,10 @@ from MPXMusic.utils.database import (
     is_on_off,
     is_served_private_chat,
 )
-from MPXMusic.utils.decorators.language import language_start
-from MPXMusic.utils.formatters import get_readable_time
-from MPXMusic.utils.functions import MARKDOWN, WELCOMEHELP
-from MPXMusic.utils.inline import private_panel, start_pannel
+from WinxMusic.utils.decorators.language import language_start
+from WinxMusic.utils.formatters import get_readable_time
+from WinxMusic.utils.functions import MARKDOWN, WELCOMEHELP
+from WinxMusic.utils.inline import private_panel, start_pannel
 from config import BANNED_USERS, START_IMG_URL
 from config.config import OWNER_ID, PREFIXES
 from strings import command, get_command, get_string
@@ -34,6 +34,49 @@ loop = asyncio.get_running_loop()
 
 START_COMMAND = get_command("START_COMMAND")
 
+IMAGE = [
+    "https://graph.org/file/f76fd86d1936d45a63c64.jpg",
+    "https://graph.org/file/69ba894371860cd22d92e.jpg",
+    "https://graph.org/file/67fde88d8c3aa8327d363.jpg",
+    "https://graph.org/file/3a400f1f32fc381913061.jpg",
+    "https://graph.org/file/a0893f3a1e6777f6de821.jpg",
+    "https://graph.org/file/5a285fc0124657c7b7a0b.jpg",
+    "https://graph.org/file/25e215c4602b241b66829.jpg",
+    "https://graph.org/file/a13e9733afdad69720d67.jpg",
+    "https://graph.org/file/692e89f8fe20554e7a139.jpg",
+    "https://graph.org/file/db277a7810a3f65d92f22.jpg",
+    "https://graph.org/file/a00f89c5aa75735896e0f.jpg",
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",  
+    "https://graph.org/file/eaa3a2602e43844a488a5.jpg",
+    "https://graph.org/file/b129e98b6e5c4db81c15f.jpg",
+    "https://graph.org/file/3ccb86d7d62e8ee0a2e8b.jpg",
+    "https://graph.org/file/df11d8257613418142063.jpg",
+    "https://graph.org/file/9e23720fedc47259b6195.jpg",
+    "https://graph.org/file/826485f2d7db6f09db8ed.jpg",
+    "https://graph.org/file/ff3ad786da825b5205691.jpg",
+    "https://graph.org/file/52713c9fe9253ae668f13.jpg",
+    "https://graph.org/file/8f8516c86677a8c91bfb1.jpg",
+    "https://graph.org/file/6603c3740378d3f7187da.jpg",
+    "https://graph.org/file/66cb6ec40eea5c4670118.jpg",
+    "https://graph.org/file/2e3cf4327b169b981055e.jpg",   
+]
 
 @app.on_message(filters.command(START_COMMAND, PREFIXES) & filters.private & ~BANNED_USERS)
 @language_start
@@ -46,7 +89,7 @@ async def start_comm(client: Client, message: Message, _):
             keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, close=True))
             if config.START_IMG_URL:
                 return await message.reply_photo(
-                    photo=START_IMG_URL,
+                    random.choice(IMAGE),
                     caption=_["help_1"],
                     reply_markup=keyboard,
                 )
@@ -207,7 +250,7 @@ async def start_comm(client: Client, message: Message, _):
         if config.START_IMG_URL:
             try:
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
+                    random.choice(IMAGE),
                     caption=_["start_1"].format(app.mention),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
